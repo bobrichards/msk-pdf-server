d0350ffa3cfbbf1d6f28e7576a61b6fd0b3e16de
Edwin Ramirez <info@mskanalysis.com>
2025-04-24 11:02:10 +0100

    Update template and server files

--- server.js
+++ server.js
@@ -66,6 +66,9 @@ app.post('/generate', upload.any(), async (req, res) => {
     if (images.length > 0) {
      imageSection = `
 	  <div align="center"> 
+	   <div class="image-container1" style="text-align: center; padding: 20px;">
+      <img src="${logoBase64}" alt="Logo" style="max-width: 300px; display: block; margin: 0 auto;" />
+    </div><br />
 	  <h2>Assessment Images</h2><br />
 	  <p>All reports are produced dynamically and no identifiable data is stored in this process</p></div><br />
   <div class="image-container" style="display: grid; grid-template-columns: repeat(2, 1fr); gap: 20px; width: 65%; margin: 40px auto;">
@@ -76,7 +79,7 @@ app.post('/generate', upload.any(), async (req, res) => {
       <br />
 	 
 	  </div>`).join('\n')}
-  </div><br /><br /> <div align="center"><p><b>End of Report</b></p></div>`;
+  </div><br />`;
 
 
 } else {
