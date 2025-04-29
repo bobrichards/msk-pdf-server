48145876793c6b0f26d793faa41f2bd3fd0c82ad
Edwin Ramirez <info@mskanalysis.com>
2025-04-29 07:31:04 +0100

    Update template and server files

--- server.js
+++ server.js
@@ -65,6 +65,7 @@ app.post('/generate', upload.any(), async (req, res) => {
     let imageSection = '';
     if (images.length > 0) {
      imageSection = `
+	 <div class="page-break"></div>
 	  <div align="center"> 
 	   <div class="image-container1" style="text-align: center; padding: 20px;">
       <img src="${logoBase64}" alt="Logo" style="max-width: 300px; display: block; margin: 0 auto;" />
@@ -84,6 +85,7 @@ app.post('/generate', upload.any(), async (req, res) => {
 
 } else {
   imageSection = `
+  <div class="page-break"></div>
     <div class="image-container1" style="text-align: center; padding: 20px;">
     </div>`;
 }
